e8acdf933df711c0ad734315bccbd275a3a5ae82
Fullstack web developer <84002031+ricardo-applepie@users.noreply.github.com>
2022-01-16 02:04:31 +0100

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import { applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import cartReducer from '../reducers/cartReducer/cartReducer';
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
-import cartReducer from '../reducers/cartReducer/cartReducer';
 export default configureStore({
   reducer: {
     cart: cartReducer,
